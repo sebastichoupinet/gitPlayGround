7aa3df8cad8900c7b9f3db0655a412e642485a18
sebastichoupinet <sebastiennoton@gmail.com>
2019-05-26 08:07:52 +0200

    Added an simplistic project

---
+++ firstFileTosee.js
@@ -0,0 +1,38 @@
+// hey local user !
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 8080,
+		host: 'localhost'
+	});
+	await server.register(require('@hapi/inert'));
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: {
+			function(request, h) {
+				return 'Hello world';
+			}
+		}
+	});
+
+	server.route({
+		method: 'GET',
+		path: '/picture',
+		handler: {
+			file: 'picture.jpg'
+		}
+	});
+	await server.start();
+	console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
